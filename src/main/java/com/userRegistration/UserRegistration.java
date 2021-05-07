57d0f4149b92b5608cb95e085e89a30c33c66ef0
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-07 20:21:46 +0530

    Added: email validation added

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -10,12 +10,22 @@ public class UserRegistration {
         String name;
         System.out.println("Welcome to User Registration");
         System.out.println("Enter your First name");
-        name = sc.nextLine();
-        nameValidation(name);
+        nameValidation(sc.nextLine());
         System.out.println("Enter your Last name");
-        name =sc.nextLine();
-        nameValidation(name);
+        nameValidation(sc.nextLine());
+        System.out.println("Enter your email adderss");
+        emailValidation(sc.nextLine());
     }
+
+    private static void emailValidation(String email) {
+        if(Pattern.matches("^[A-Za-z0-9_][A-Za-z0-9_]*[@]{1}[a-z]+[.]{1}[a-z]{2,6}$", email)){
+            System.out.println("valid");
+        }
+        else {
+            System.out.println("Please enter valid email address");
+        }
+    }
+
     private static final void nameValidation(String name){
         if(Pattern.matches("[A-Z][a-z]{2,}", name)){
             System.out.println("valid");

2b481ee0674b4e7f1126378a213d58629f4ae56b
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-07 19:26:56 +0530

    Welcome to user registration

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -1,5 +1,8 @@
 package com.userRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");

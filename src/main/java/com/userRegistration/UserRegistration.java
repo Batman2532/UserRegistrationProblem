0237d9c82c4eb372934b1925ed9024dea41c7aea
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-07 20:34:01 +0530

    Added: email validation added

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -7,14 +7,26 @@ public class UserRegistration {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String name;
-        System.out.println("Welcome to User Registration");
-        System.out.println("Enter your First name");
-        nameValidation(sc.nextLine());
-        System.out.println("Enter your Last name");
-        nameValidation(sc.nextLine());
-        System.out.println("Enter your email adderss");
-        emailValidation(sc.nextLine());
+        while(true) {
+            System.out.println("Welcome to User Registration");
+            System.out.println("Enter your First name");
+            nameValidation(sc.nextLine());
+            System.out.println("Enter your Last name");
+            nameValidation(sc.nextLine());
+            System.out.println("Enter your email address");
+            emailValidation(sc.nextLine());
+            System.out.println("Enter your mobile number");
+            mobileNumberValidation(sc.nextLine());
+        }
+    }
+
+
+    private static void mobileNumberValidation(String mobileNumber) {
+        if(Pattern.matches("^[1-9]{1,2}[ ]{1}[1-9]{1}[0-9]{9}$", mobileNumber)){
+            System.out.println("valid");
+        }else {
+            System.out.println("please enter a valid number");
+        }
     }
 
     private static void emailValidation(String email) {
@@ -27,7 +39,7 @@ public class UserRegistration {
     }
 
     private static final void nameValidation(String name){
-        if(Pattern.matches("[A-Z][a-z]{2,}", name)){
+        if(Pattern.matches("[A-Z][a-z]{3,}", name)){
             System.out.println("valid");
         }else{
             System.out.println("Please enter first letter capital ");

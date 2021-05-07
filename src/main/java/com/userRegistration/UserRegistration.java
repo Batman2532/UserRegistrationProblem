e8578c6bb22935c6f509940521cf0c4c35693edf
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-07 19:34:02 +0530

    Added: validation for first name

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static final void firstName(){
+
+        String firstName = sc.nextLine();
+        if(Pattern.matches("[A-Z]{1}[a-z]*", firstName)){
+            System.out.println("Enter your last name");
+        }else{
+            System.out.println("Please enter first letter capital ");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
+        System.out.println("Enter your First name");
+        firstName();
     }
 }

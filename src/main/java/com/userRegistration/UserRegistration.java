6e06dd05a7c983cb7a748f0fcb8ce7ce9570fc06
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-07 20:00:10 +0530

    Added: validation for last name

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -6,18 +6,22 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static Scanner sc = new Scanner(System.in);
 
-    private static final void firstName(){
-
-        String firstName = sc.nextLine();
-        if(Pattern.matches("[A-Z]{1}[a-z]*", firstName)){
-            System.out.println("Enter your last name");
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Welcome to User Registration");
+        System.out.println("Enter your First name");
+        name = sc.nextLine();
+        nameValidation(name);
+        System.out.println("Enter your Last name");
+        name =sc.nextLine();
+        nameValidation(name);
+    }
+    private static final void nameValidation(String name){
+        if(Pattern.matches("[A-Z][a-z]{2,}", name)){
+            System.out.println("valid");
         }else{
             System.out.println("Please enter first letter capital ");
         }
     }
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration");
-        System.out.println("Enter your First name");
-        firstName();
-    }
+
 }

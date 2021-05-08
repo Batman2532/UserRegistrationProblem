9fa0e068efbc4f6176b4e731f6f5f73502a25ab1
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-08 11:00:32 +0530

    Added: password must contain one capital letter

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -8,22 +8,22 @@ public class UserRegistration {
 
     public static void main(String[] args) {
         while(true) {
-            System.out.println("Welcome to User Registration");
-            System.out.println("Enter your First name");
-            nameValidation(sc.nextLine());
-            System.out.println("Enter your Last name");
-            nameValidation(sc.nextLine());
-            System.out.println("Enter your email address");
-            emailValidation(sc.nextLine());
-            System.out.println("Enter your mobile number");
-            mobileNumberValidation(sc.nextLine());
+//            System.out.println("Welcome to User Registration");
+//            System.out.println("Enter your First name");
+//            nameValidation(sc.nextLine());
+//            System.out.println("Enter your Last name");
+//            nameValidation(sc.nextLine());
+//            System.out.println("Enter your email address");
+//            emailValidation(sc.nextLine());
+//            System.out.println("Enter your mobile number");
+//            mobileNumberValidation(sc.nextLine());
             System.out.println("Enter password it must be one capital letter, one special character and one number");
             passwordValidation(sc.nextLine());
         }
     }
 
     private static void passwordValidation(String password) {
-        if(Pattern.matches("^[a-z]{8,20}",password)){
+        if(Pattern.matches("^([A-Za-z]{8,20})$",password)){
             System.out.println("valid");
         }else {
             System.out.println("Entered password is invalid");

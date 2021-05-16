ee0dec87e021462820bc381147c2ac8faab5d775
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-16 16:15:55 +0530

    Added: lambda function added

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -4,60 +4,42 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface  validateUser {
+        boolean validate(String pattern, String userDetails);
+}
+
 public class UserRegistration {
     private static Scanner sc = new Scanner(System.in);
+    public static boolean resultValue;
+    public static final String NAMEPATTERN = "[A-Z][a-z]{3,}";
+    public static final String LASTNAMEPATTERN = "[A-Z][a-z]{3,}";
+    public static final String EMAILPATTERN = "^abc[a-zA-Z0-9.+-]*@[a-z]*[.][a-z]{2,5}[.,a-z]{0,5}";
+    public static final String PHONEPATTERN = "^[1-9]{1,3}[ ]{1}[1-9]{1}[0-9]{9}$";
+    public static final String PASSWORDPATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-d+=()])(?=\\S+$).{8,20}$";
 
     public static void main(String[] args) throws UserRegistrationException {
-        while(true) {
+        validateUser validator = (String userInput, String pattern) ->  Pattern.matches( pattern,userInput);
+        try {
             System.out.println("Welcome to User Registration");
             System.out.println("Enter your First name");
-            nameValidation(sc.nextLine());
+            System.out.println("Valid Details " + validator.validate(sc.next(), NAMEPATTERN));
             System.out.println("Enter your Last name");
-            nameValidation(sc.nextLine());
+            System.out.println("Valid Details " + validator.validate(sc.next(), NAMEPATTERN));
             System.out.println("Enter your email address");
-            emailValidation(sc.nextLine());
+            System.out.println("Valid Details " + validator.validate(sc.next(), EMAILPATTERN));
             System.out.println("Enter your mobile number");
-            mobileNumberValidation(sc.nextLine());
+            System.out.println("Valid Details " + validator.validate(sc.next(), PHONEPATTERN));
             System.out.println("Enter password it must be one capital letter, one special character and one number");
-            passwordValidation(sc.nextLine());
+            System.out.println("Valid Details " + validator.validate(sc.next(), PASSWORDPATTERN));
+        }catch (UserRegistrationException)
+        {
+
+        }
         }
     }
 
-    public static boolean passwordValidation(String password) throws UserRegistrationException {
-        if(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-d+=()])(?=\\S+$).{8,20}$",password)){
-            System.out.println("valid");
-            return true;
-        }else {
-            throw new UserRegistrationException("Enter valid password");
-        }
-    }
 
-    public static boolean mobileNumberValidation(String mobileNumber) throws UserRegistrationException {
-        if(Pattern.matches("^[1-9]{1,3}[ ]{1}[1-9]{1}[0-9]{9}$", mobileNumber)){
-            System.out.println("valid");
-            return true;
-        }else {
-            throw new UserRegistrationException("Enter valid phone number");
-        }
-    }
 
-    public static boolean emailValidation(String email) throws UserRegistrationException {
-        if(Pattern.matches("^abc[a-zA-Z0-9.+-]*@[a-z]*[.][a-z]{2,5}[.,a-z]{0,5}", email)){
-            System.out.println("valid");
-            return true;
-        }else {
-            throw new UserRegistrationException("Enter valid email");
-        }
-    }
-
-    public static boolean nameValidation(String name) throws UserRegistrationException{
-            if (Pattern.matches("[A-Z][a-z]{3,}", name)) {
-                System.out.println("valid");
-                return true;
-            }else {
-                throw new UserRegistrationException("Enter valid name");
-            }
-    }
-}
 
 
